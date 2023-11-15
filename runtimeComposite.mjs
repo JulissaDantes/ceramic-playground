97f14a3873cc7b6e7981c1376e016fa316537878
JulissaDantes <julissadcj@gmail.com>
2023-11-15 09:39:36 -0400

    add create runtime composite file

---
+++ runtimeComposite.mjs
@@ -0,0 +1,12 @@
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import { writeEncodedCompositeRuntime } from '@composedb/devtools-node'
+
+// Replace by the URL of the Ceramic node you want to deploy the models to
+const ceramic = new CeramicClient('http://localhost:7007')
+
+// Replace by the path to the local encoded composite file
+await writeEncodedCompositeRuntime(
+  ceramic,
+  'my-composite.json',
+  '__generated__/definition.js'
+)
