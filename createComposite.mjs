f09e43739588fb642799f8a26342b96ec7c51a86
JulissaDantes <julissadcj@gmail.com>
2023-11-15 07:48:02 -0400

    add model

--- createComposite.mjs
+++ createComposite.mjs
@@ -6,7 +6,12 @@ import { getResolver } from 'key-did-resolver';
 // Import the devtool node package
 import { createComposite, writeEncodedComposite } from '@composedb/devtools-node';
 
-const seed = new Uint8Array([157, 243, 44, 109, 192, 15, 72, 51, 96, 235, 41, 250, 4, 164, 114, 243, 44, 109, 192, 15, 72, 51, 96, 235, 41, 250, 4, 164, 15, 72, 51, 96]);
+const seed = new Uint8Array([
+    192,  16, 89, 183,  66, 111,  35,  98,
+    211, 155, 35, 149, 177, 242, 119,  55,
+    202,  79, 94, 168, 106,  74,  17,   0,
+    116, 105, 77, 116, 161, 176,  81, 189
+  ]);
 const provider = new Ed25519Provider(seed);
 const did = new DID({ provider, resolver: getResolver() });
 
@@ -18,7 +23,7 @@ const ceramic = new CeramicClient('http://localhost:7007');
 ceramic.did = did;
 
 // Replace by the path to the source schema file
-const composite = await createComposite(ceramic, './models/my_schema.graphql');
-
+const composite = await createComposite(ceramic, './models/my-test-schema.graphql');
+console.log("1");
 // Replace by the path to the encoded composite file
-await writeEncodedComposite(composite, './my-composite.json');
+await writeEncodedComposite(composite, './my-composite.json');console.log("1");
