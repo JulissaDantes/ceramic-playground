40e5878b3f4295515a1e80879e63d10a97467e1b
JulissaDantes <julissadcj@gmail.com>
2024-01-10 13:48:58 -0400

    setting env

--- modelWorkflow.mjs
+++ modelWorkflow.mjs
@@ -5,9 +5,9 @@ import { getResolver } from 'key-did-resolver';
 // Import the devtool node package
 import { createComposite, writeEncodedComposite, writeEncodedCompositeRuntime } from '@composedb/devtools-node';
 
-const modelFile = "00-basicProfile.graphql";
-const compositeFile = "my-composite-single.json";
-const definitionFile = "__generated__/definition-single.js";
+const modelFile = "failing1.graphql";
+const compositeFile = "my-composite-fail1.json";
+const definitionFile = "__generated__/definition-fail1.js";
 const seed = new Uint8Array([//Random numbers
     192,  16, 89, 183,  66, 111,  35,  98,
     211, 155, 35, 149, 177, 242, 119,  55,
