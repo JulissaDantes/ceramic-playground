db462e8a87ace3750b78970b272799181e0bb287
JulissaDantes <julissadcj@gmail.com>
2024-01-11 17:43:27 -0400

    Change repo structure

--- modelWorkflow.mjs
+++ modelWorkflow.mjs
@@ -5,9 +5,9 @@ import { getResolver } from 'key-did-resolver';
 // Import the devtool node package
 import { createComposite, writeEncodedComposite, writeEncodedCompositeRuntime } from '@composedb/devtools-node';
 
-const modelFile = "failing1.graphql";
-const compositeFile = "my-composite-fail1.json";
-const definitionFile = "__generated__/definition-fail1.js";
+const modelFile = "2.graphql";
+const compositeFile = "my-composite-2.json";
+const definitionFile = "__generated__/definition-2.js";
 const seed = new Uint8Array([//Random numbers
     192,  16, 89, 183,  66, 111,  35,  98,
     211, 155, 35, 149, 177, 242, 119,  55,
@@ -25,13 +25,13 @@ const ceramic = new CeramicClient('http://localhost:7007');
 ceramic.did = did;
 
 const composite = await createComposite(ceramic, './models/' + modelFile);
-
+console.log("Created composite for model:", modelFile);
 await writeEncodedComposite(composite, './' + compositeFile);
-
+console.log("Created encoded composite")
 await writeEncodedCompositeRuntime(
     ceramic,
     compositeFile,
     definitionFile
 )
-
-console.log("SUCCESS");
+console.log("Deployed composite")
+console.log("SUCCESS ", );
